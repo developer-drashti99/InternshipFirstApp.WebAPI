2b1bf5b38e11fc572a5d6091882fcdb94e4d31a4
Drashti <developer.drashtikariya@gmail.com>
2026-02-05 16:45:53 +0530

    added multiple themes to customize website using daisyUI themes

--- client/src/layout/nav-bar/nav-bar.ts
+++ client/src/layout/nav-bar/nav-bar.ts
@@ -1,8 +1,9 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service.service';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { ToastService } from '../../core/services/toast-service.service';
+import { themes } from '../themes';
 
 @Component({
   selector: 'app-nav-bar',
@@ -10,12 +11,27 @@ import { ToastService } from '../../core/services/toast-service.service';
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css',
 })
-export class NavBar {
+export class NavBar implements OnInit {
   
   protected accountService = inject(AccountService);
   private route=inject(Router);
   private toast=inject(ToastService);
   protected credentials: { email: string; password: string } = { email: '', password: '' };
+  protected selectedTheme=signal<string>(localStorage.getItem("theme")||"light");
+  protected themes=themes;
+  
+  ngOnInit(): void {
+      document.documentElement.setAttribute('data-theme',this.selectedTheme());
+  }
+
+  handleSelectedTheme(theme:string)
+  {
+    this.selectedTheme.set(theme);
+    localStorage.setItem("theme",theme);
+    document.documentElement.setAttribute('data-theme',theme);
+    const elem=document.activeElement as HTMLDivElement;// themes dropdown which is currently active or we can say open
+    if(elem) elem.blur();//for closing dropdown after selecting theme
+  }
   
   onLogin() {
     this.accountService.login(this.credentials).subscribe({
---
+++ client/src/layout/themes.ts
@@ -0,0 +1,26 @@
+export const themes = [
+"light", "dark",
+"cupcake",
+"bumblebee",
+"emerald",
+"corporate",
+"synthwave",
+"retro",
+"cyberpunk",
+"valentine",
+"halloween",
+"garden",
+"forest",
+"aqua",
+"lofi",
+"pastel",
+"fantasy",
+"wireframe",
+"black",
+"luxury",
+"dracula"
+]
+export const themeBnW=[
+  "light",
+  "dark"
+]
