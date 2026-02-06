54ef2a700596d79dcae52c777c4e3ba4f1dfd0d6
Drashti <developer.drashtikariya@gmail.com>
2026-02-06 14:46:27 +0530

    added sample caching

--- client/src/core/interceptors/loading-interceptor.ts
+++ client/src/core/interceptors/loading-interceptor.ts
@@ -1,13 +1,26 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpEvent, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { BusyService } from '../services/busy-service.service';
-import { delay, finalize } from 'rxjs';
-
+import { delay, finalize, of, tap } from 'rxjs';
+// adding caching
+const cache = new Map<string, HttpEvent<unknown>>();//HttpEvent<unknown> is type of response
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
   const busyService = inject(BusyService);
+
+  if (req.method === 'GET') {
+    const cachedResponse = cache.get(req.url);
+    if (cachedResponse) {
+      return of(cachedResponse);
+    }
+  }
+
   busyService.busy();
+
   return next(req).pipe(
     delay(500),
+    tap(response=>{
+      cache.set(req.url,response)
+    }),
     finalize(() => {
       busyService.idle()
     })
